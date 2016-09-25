c3784eb0a0b10b44d18c5bf63576867d4f6e565d
Arekseon <petroys@yandex.ru>
2016-09-24 19:38:20 -0700

    asdf

--- login_page_prototype/app.py
+++ login_page_prototype/app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-import socket, Database, time
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
+import socket, Database, time, os
 from functools import wraps
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 global username 
 username = ''
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "your_moms"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 
 def login_required(f):
@@ -21,14 +29,19 @@ def login_required(f):
             return redirect(url_for('login'))
     return wrap
 
-
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    return render_template('menu.html', username=username)
+
+
+@app.route('/notes', methods=['GET', 'POST'])
+@login_required
+def hello():
     global username
     if request.method == 'GET':
         notes = Database.get_notes_by_username(username)
-        return render_template('hello.html', username = username, notes = notes)
+        return render_template('notes.html', username = username, notes = notes)
     if request.method == 'POST':
         
         if(request.form.get('update')) == 'add':
@@ -39,7 +52,7 @@ def index():
             Database.delete_note_by_id(note_to_delete)
 
         notes = Database.get_notes_by_username(username)
-        return render_template('hello.html', username = username, notes = notes)
+        return render_template('notes.html', username = username, notes = notes)
 
 
 @app.route('/test_stuff', methods=['GET', 'POST'])
@@ -88,6 +101,66 @@ def logout():
         session.pop('logged_in', None)
         return redirect(url_for('login'))
 
+
+
+# @app.route('/upload_file', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return redirect(url_for('uploaded_file',
+#                                     filename=filename))
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form action="" method=post enctype=multipart/form-data>
+#       <p><input type=file name=file>
+#          <input type=submit value=Upload>
+#     </form>
+#     '''
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+
+@app.route('/pics', methods=['GET', 'POST'])
+@login_required
+def pics():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            print "###################1"
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            print "###################2"
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('pics',
+            #                         filename=filename))
+    return render_template('pics.html', username = username)
 
 
 def check_for_valid_user(username, password):
